974dab3246c9c6230c7c11d0f2c0599f5cb8a092
HudsonSalles <hudsonsalless@gmail.com>
2024-10-13 21:30:27 -0300

    feat(Faq): Animation on answers

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+
+//internal components
 import { Container } from "@/components/Container";
+
+//components
 import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
 } from "@headlessui/react";
+import { motion } from "framer-motion";
+
+//icons
 import { ChevronUpIcon } from "@heroicons/react/24/solid";
 
 export const Faq = () => {
@@ -18,13 +25,19 @@ export const Faq = () => {
                 <>
                   <DisclosureButton className="flex items-center justify-between w-full px-4 py-4 text-lg text-left text-gray-800 rounded-lg bg-gray-50 hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-100 focus-visible:ring-opacity-75 dark:bg-trueGray-800 dark:text-gray-200">
                     <span>{item.question}</span>
-                    <ChevronUpIcon
-                      className={`${
-                        open ? "transform rotate-180" : ""
-                      } w-5 h-5 text-indigo-500`}
-                    />
+                    <motion.div
+                      animate={{ rotate: open ? 180 : 0 }}
+                      transition={{ duration: 0.3, ease: "easeInOut" }}
+                    >
+                      <ChevronUpIcon className="w-5 h-5 text-indigo-500" />
+                    </motion.div>
                   </DisclosureButton>
-                  <DisclosurePanel className="px-4 pt-4 pb-2 text-gray-500 dark:text-gray-300">
+                  <DisclosurePanel
+                    as={motion.div}
+                    initial={{ height: 0 }}
+                    animate={{ height: open ? "auto" : 0 }}
+                    className="mt-2 px-4 text-gray-500 dark:text-gray-300"
+                  >
                     {item.answer}
                   </DisclosurePanel>
                 </>
